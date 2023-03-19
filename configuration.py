941e149c1cc617544ba87b7f542ef5ad918b2585
Natalia Mironova <natalymir82@gmail.com>
2023-03-19 10:37:33 +0300

    README изменён

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = 'https://1ed39350-0219-4f7c-9a00-f6a8daeb7942.serverhub.praktikum-services.ru'
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+CREATE_KITS_PATH = "/api/v1/kits"
