10c0b445691fbfc805237f0ebdf3e398c6015f07
Natalia Mironova <natalymir82@gmail.com>
2023-03-19 11:29:38 +0300

    удалён лишний закомментированный код

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -5,8 +5,6 @@ import data
 
 def get_users_table():
     return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
-# response = get_users_table()
-# print(response.status_code)
 
 
 # Создаётся новый пользователь
@@ -14,10 +12,6 @@ def post_new_user(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH, #подставляем полный URL
                          json=body,  # тут тело
                          headers=data.headers)  # а здесь заголовки
-
-# user_response = post_new_user(data.user_body)
-# print(user_response.json())
-# print(user_response.status_code)
 
 
 #  Создаётся новый набор с authToken созданного пользователя
